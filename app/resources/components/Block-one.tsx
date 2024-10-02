f8051026dc567f0222fa2e212cfda17c23ddeeef
ElieMaged <Narutossj23@yahoo.com>
2024-10-02 14:38:09 +0300

    qwe

--- app/resources/components/Block-one.tsx
+++ app/resources/components/Block-one.tsx
@@ -1,33 +1,31 @@
 export default function BlockOne() {
     return(
-        <div className='grid grid-cols-9 grid-rows-9 text-white '>
-<div className='bg-red-700 col-start-1 col-end-7 row-start-1 row-end-6'>
-   <h1>Interior Design</h1>
-   <p>Step into a world where the art of Interior Design is meticulously crafted to bring together timeless elegance and cutting-edge 
-   modern Innovation, Allowing you to transform your living spaces into the epitome of luxury and sophistication</p>
+
+  <div className='flex flex-col md:grid md:grid-cols-6 md:grid-rows-6'>
+    {/* first block */}
+    <div id='squareOne' className='bg-white text-black p-5 md:col-start-1 md:col-end-5'>
+<h1>Interior Design</h1>
+<p>Step into a world where the art of Interior Design is meticulously crafted to bring together timeless elegance and cutting-edge 
+modern Innovation, Allowing you to transform your living spaces into the epitome of luxury and sophistication</p>
 </div>
-<div className='bg-blue-700 col-start-1 col-end-6 row-start-6 row-end-10'>
-    <div className='flex flex-row items-start gap-x-6'>
-   
-        <br />
-        <br />
-        <br />
-    <button>Start Project</button>
-    <br />
-    <div className='flex flex-row'>
-        <h1>400+</h1>
-        <h1>400+</h1>
-        </div>
-    </div>
-   
+<div id='squareTwo' className=' md:col-start-5 md:col-end-7 '>
+<img className='square-two-img flex flex-row' src='https://i.imgur.com/6GXUGn6.png' />
 </div>
-<div className='bg-green-700  col-start-7 col-end-10 row-start-1 row-end-6'>
-    WEQWE
+<div id='squareThree' className='p-5 md:col-start-1 md:col-end-4 bg-white '>
+<button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 ">Start Project</button>
+<span className='flex flex-row gap-x-14 md:gap-x-24 text-center text-black bg-white'>
+ 
+    <div>400+<p>Project Complete</p></div>
+    <div>600+<p>Satisfied Clients</p></div>
+    <div>100+<p>Unique Styles</p></div>
+
+</span>
 </div>
-<div className='bg-yellow-700  col-start-6 col-end-10 row-start-6 row-end-10'>
-    WEQWE
+<div  id='squareFour' className='md:col-start-4 md:col-span-4 md:flex flex-col'>
+    <img className='' src='https://i.imgur.com/AZtmdbe.png' />
 </div>
 
-        </div>
+  </div>
+  
     )
 }
